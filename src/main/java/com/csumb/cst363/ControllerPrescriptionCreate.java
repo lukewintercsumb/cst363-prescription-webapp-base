5582f48076e69914340c9814ac3aead2aa341da6
tomasdiazwahl <tdiazwahl@csumb.edu>
2024-05-06 19:51:35 -0700

    prescription create theoretically done

--- src/main/java/com/csumb/cst363/ControllerPrescriptionCreate.java
+++ src/main/java/com/csumb/cst363/ControllerPrescriptionCreate.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.SortedMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -52,20 +53,96 @@ public class ControllerPrescriptionCreate {
 		 * 6. Get generated value for rxid 
 		 * 7. Update prescription object and return
 		 */
-		//Obtain connection to database.
-		Connection pConnection = null;
-		try {
-			pConnection= getConnection();
-		} catch (NullPointerException | SQLException e){
-			System.out.println("Not great thing happened. UH OH.");
-		}
-		//Validate that doctor id and name exists
-		PreparedStatement preparedStatement = pConnection.prepareStatement("SELECT doctor_id, name " +
+
+		Connection connection = null;
+		ResultSet rs = null;
+		String query = "SELECT doctor_id, first_name, last_name" +
 				"FROM doctor " +
-				"WHERE doctor_id = ? AND name = ?");
-		preparedStatement.setInt(1, p.getDoctor_id());
-		//TODO fix this
-		preparedStatement.setString(1,p.getDoctorFirstName() + " " + p.getDoctorLastName());
+				"WHERE doctor_id = ?";
+		try {
+			//Obtain connection to database.
+			connection = getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, p.getDoctor_id());
+			// Validate that doctor id and name exists
+			rs = preparedStatement.executeQuery();
+			if (!rs.next()) {
+				model.addAttribute("message", "Dr."
+						+ p.getDoctorLastName() + " ID: "
+						+ p.getDoctor_id() + " could not be found");
+				model.addAttribute("prescription", p);
+				return "prescription_create";
+			}
+			query = "SELECT patient_id, first_name, last_name" +
+					"FROM patient " +
+					"WHERE patient_id = ?";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, p.getPatient_id());
+			// Validate that patient id and name exists
+			rs = preparedStatement.executeQuery();
+
+			if (!rs.next()) {
+				model.addAttribute("message", "Patient: "
+						+ p.getPatientLastName() + " ID: "
+						+ p.getPatient_id() + " could not be found");
+				model.addAttribute("prescription", p);
+				return "prescription_create";
+			}
+
+			query = "SELECT drug_id, drug_name" +
+					"FROM drug" +
+					"WHERE drug_name = ?";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, p.getDrugName());
+			// Validate that patient id and name exists
+			rs = preparedStatement.executeQuery();
+
+			if (!rs.next()) {
+				model.addAttribute("message", "Drug: "
+						+ p.getDrugName() + " ID could not be found");
+				model.addAttribute("prescription", p);
+				return "prescription_create";
+			}
+
+			//Insert new prescription
+			int drug_id = rs.getInt("drug_id");
+
+			query = "INSERT into prescription(prescribing_doctor_id, prescription_patient_id, drug_id, " +
+					"drug_quantity, max_refills, total_cost) values (?,?,?,?,?,?)";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, p.getDoctor_id());
+			preparedStatement.setInt(2, p.getPatient_id());
+			preparedStatement.setInt(3, drug_id);
+			preparedStatement.setInt(4, p.getQuantity());
+			preparedStatement.setInt(5, p.getRefills());
+			preparedStatement.setString(6, p.getCost());
+
+			rs = preparedStatement.executeQuery();
+
+			//Get generated value for rxid
+			query = "SELECT rxid FROM prescription WHERE prescribing_doctor_id = ? AND prescribing_patient_id = ?" +
+					" AND drug_id = ?";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, p.getDoctor_id());
+			preparedStatement.setInt(2, p.getPatient_id());
+			preparedStatement.setInt(3, drug_id);
+
+			rs = preparedStatement.executeQuery();
+
+			// Update prescription object and return
+			p.setRxid(rs.getString("rxid"));
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// close all resources
+				if (rs != null) rs.close();
+				if (connection != null) connection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
 
 		//BOILER CODE
 		model.addAttribute("message", "Prescription created.");
